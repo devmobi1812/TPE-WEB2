7abbafe038a12c24c3557501a5c3c3041006e952
Dario213987 <dariocrsng@gmail.com>
2024-10-09 17:30:59 -0300

    Creada implementación del AuthHelper

--- helpers/AuthHelper.php
+++ helpers/AuthHelper.php
@@ -1,5 +1,34 @@
 <?php 
+require_once "app/models/UsuariosModel.php";
     class AuthHelper{
-        
+        private static $userModel;
+
+        public static function loadModel(){
+            if(self::$userModel == null){
+                self::$userModel = new UsuariosModel();
+            }
+        }
+        public static function login($user){
+            self::loadModel();
+            $fetchedUser = self::$userModel->findByUsername($user->nombre);
+            if(!empty($fetchedUser)){
+                if(password_verify($user->password, $fetchedUser->password)){
+                    session_start();
+                    $_SESSION["user"] = $fetchedUser; 
+                }
+            }
+        }
+        public static function loggedUser(){
+            if(session_status() == PHP_SESSION_ACTIVE){
+                return $_SESSION["user"];
+            }else{
+                return false;
+            }
+        }
+
+        public static function isAdmin(){
+            $user = self::loggedUser();
+            return ($user) ? $user->rol_nombre == "ADMIN" : $user;
+        }
     }
 ?>
